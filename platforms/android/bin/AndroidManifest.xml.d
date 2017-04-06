4b878e599497de95c05c7cf9c7b0fef8a3f18c68
mpallansch <mkp95@uga.edu>
2017-04-06 14:10:21 -0400

    Fixes Git Ignore

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\matt\exampleproject\platforms\android\bin\AndroidManifest.xml : \
-C:\Users\matt\exampleproject\platforms\android\AndroidManifest.xml \
--- platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\matt\exampleproject\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
-C:\Users\matt\exampleproject\platforms\android\bin\HelloWorld.ap_ \
-C:\Users\matt\exampleproject\platforms\android\bin\classes.dex \
-C:\Users\matt\exampleproject\platforms\android\CordovaLib\bin\classes.jar \
